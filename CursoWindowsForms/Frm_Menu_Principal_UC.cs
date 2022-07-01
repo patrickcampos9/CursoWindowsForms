d273e1f8c8adaf8d5ee5f1ad59b46c51a141574b
Patrick de Campos Nascimento <patrickcampos77@hotmail.com>
2022-07-01 16:53:21 -0300

    criacao forms user controls

---
+++ CursoWindowsForms/Frm_Menu_Principal_UC.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Menu_Principal_UC : Form
+    {
+        int ControleHelloWorld = 0;
+        int ControleDemonstracaoKey = 0;
+        int ControleMascara = 0;
+        int ControleValidaCPF = 0;
+        int ControleValidaCPF2 = 0;
+        int ControleValidaSenha = 0;
+        public Frm_Menu_Principal_UC()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleHelloWorld += 1;
+            Frm_HelloWorld_UC U = new Frm_HelloWorld_UC();
+
+            TabPage TB = new TabPage();
+            TB.Name = "Hello World " + ControleHelloWorld;
+            TB.Text = "Hello World " + ControleHelloWorld;
+            TB.ImageIndex = 1;
+            TB.Controls.Add(U);
+
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleDemonstracaoKey += 1;
+            Frm_DemonstracaoKey_UC U = new Frm_DemonstracaoKey_UC();
+            U.Dock = DockStyle.Fill;
+            TabPage TB = new TabPage();
+            TB.Name = "Demonstração Key " + ControleDemonstracaoKey;
+            TB.Text = "Demonstração Key " + ControleDemonstracaoKey;
+            TB.ImageIndex = 0;
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void mascaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleMascara += 1;
+            Frm_Mascara_UC U = new Frm_Mascara_UC();
+            U.Dock = DockStyle.Fill;
+            TabPage TB = new TabPage();
+            TB.Name = "Máscara " + ControleMascara;
+            TB.Text = "Máscara " + ControleMascara;
+            TB.ImageIndex = 2;
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleValidaCPF += 1;
+            Frm_ValidaCPF_UC U = new Frm_ValidaCPF_UC();
+            U.Dock = DockStyle.Fill;
+            TabPage TB = new TabPage();
+            TB.Name = "Valida CPF " + ControleValidaCPF;
+            TB.Text = "Valída CPF " + ControleValidaCPF;
+            TB.ImageIndex = 3;
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleValidaCPF2 += 1;
+            Frm_ValidaCPF2_UC U = new Frm_ValidaCPF2_UC();
+            U.Dock = DockStyle.Fill;
+            TabPage TB = new TabPage();
+            TB.Name = "Valida CPF2 " + ControleValidaCPF2;
+            TB.Text = "Valída CPF2 " + ControleValidaCPF2;
+            TB.ImageIndex = 4;
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleValidaSenha += 1;
+            Frm_ValidaSenha_UC U = new Frm_ValidaSenha_UC();
+            U.Dock = DockStyle.Fill;
+            TabPage TB = new TabPage();
+            TB.Name = "Valida Senha " + ControleValidaSenha;
+            TB.Text = "Valída Senha " + ControleValidaSenha;
+            TB.ImageIndex = 5;
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void apagarAbaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!(Tbc_Aplicacoes.SelectedTab == null))
+            {
+                Tbc_Aplicacoes.TabPages.Remove(Tbc_Aplicacoes.SelectedTab);
+            }
+        }
+    }
+}
